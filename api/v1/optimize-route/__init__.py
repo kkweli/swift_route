bd8f1dd1f79fc02a112c904b167625f27c55fec5
Wanjohi, George <wanjohi_gm@live.com>
2025-11-17 20:19:59 +0300

    Clean up unnecessary files - remove old lib/ directory and test files

--- api/v1/optimize-route/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SwiftRoute FastAPI Route Optimization Service
-B2B API for route optimization using Graph Neural Networks
-"""
-
-__version__ = "1.0.0"
-__author__ = "SwiftRoute Team"
-__description__ = "B2B Route Optimization API powered by Graph Neural Networks"
